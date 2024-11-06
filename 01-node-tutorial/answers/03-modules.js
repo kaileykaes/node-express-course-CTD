f09f15b89d180f3b061b5673f428cbaddeaebe60
Kailey Kaes <kaileykaes@gmail.com>
2024-11-06 12:35:18 -0700

    solve more module problems

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,8 +1,14 @@
+const { singlePerson } = require('../06-alternative-flavor');
 const names = require('./04-names');
 const sayHi = require ('./05-utils');
+const data = require ('./06-alternative-flavor');
 
 console.log(names);
+console.log(data);
+
 sayHi('Susan');
 sayHi('Peter');
 sayHi(names.arthur);
 sayHi(names.joshua);
+
+console.log(singlePerson)
---
+++ 01-node-tutorial/answers/06-alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exports.items = ['element', 'element1'];
+const person = {
+  name: 'Greg', 
+};
+
+module.exports.singlePerson = person
