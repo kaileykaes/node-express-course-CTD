751b19e44f91e7a875b77fd2a5a5f44c44ba7c69
Kailey Kaes <kaileykaes@gmail.com>
2024-11-05 17:56:13 -0700

    solve modules

---
+++ 01-node-tutorial/answers/03-modules.js
@@ -0,0 +1,8 @@
+const names = require('./04-names');
+const sayHi = require ('./05-utils');
+
+console.log(names);
+sayHi('Susan');
+sayHi('Peter');
+sayHi(names.arthur);
+sayHi(names.joshua);
---
+++ 01-node-tutorial/answers/04-names.js
@@ -0,0 +1,8 @@
+//local
+const secret = 'SUPER SECRET'
+// share
+const arthur = 'Arthur';
+const joshua = 'Joshua';
+module.exports = {arthur, joshua};
+
+console.log(module)
